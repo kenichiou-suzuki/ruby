84061b619c0a5ffe950cdcad69d45512d3dd1d52
鈴木 健一朗 <superken4396@gmail.com>
2019-11-30 18:04:53 +0000

    2019/12/01

--- hello.rb
+++ hello.rb
@@ -1,3 +1,24 @@
 puts "hello ruby !!!!!!"
 p "Branding Engineer","Hello","HR Tech"
 puts "1","2","3"
+puts "初めてのRuby"
+print "Ruby"
+puts "始めました"
+puts 3 + 3
+puts 3 - 3
+puts 3 * 3
+puts 3 / 3
+num_array = [1,2,3,4]
+p num_array
+ken = { name:"kenichirou suzuki",birthday:"19920429",bloodtype:"B"}
+puts ken
+
+def plus_ruby(abc)
+  puts abc+"ruby"
+end
+plus_ruby("初めての")
+
+def plus_one(abc)
+  puts abc+1
+end
+plus_one(15)
