0314969ac2f4d6d6985efa9884f9d32b7f2d96c7
鈴木 健一朗 <superken4396@gmail.com>
2019-12-06 16:34:08 +0000

    進捗

--- hello.rb
+++ hello.rb
@@ -22,3 +22,81 @@ def plus_one(abc)
   puts abc+1
 end
 plus_one(15)
+
+
+num = 3
+if num == 3
+  p 'numは３です'
+end
+
+num = 4
+if num > 2 && num <5
+  p 'numは３か４のどちらかです'
+end
+
+num = 3
+if num == 3
+  p 'numは３です'
+elsif num == 4
+  p 'numは４です'
+end
+
+num = 0
+while num < 5 do
+  p 'こんにちは'
+  num += 1
+end
+
+[1,2,3,4,5].each do |num|
+  p num
+end
+
+n = 9
+if n == 3
+  p '3です'
+elsif n == 4
+  p '4です'
+else
+  p 'それ以外です'
+end
+
+class People
+  
+  def initialize
+    p "Peopleのインスタンスが作られました"
+  end
+  
+  def self.greet
+    p "私はPeopleクラスです"
+  end
+  
+  def name=(value)
+    @name = value
+  end
+  
+  def name
+    @name
+  end
+  
+  attr_accessor :name
+  
+end
+
+People.greet
+
+people = People.new
+people.name = "ken"
+p people.name
+
+people2 = People.new
+people2.name = "akina"
+p people2.name
+
+class ChildPeople < People
+  
+  def self.coment
+    p "私は目からビームが出せます"
+  end
+end
+
+ChildPeople.coment
